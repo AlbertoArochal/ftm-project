0d686e7841b68bb49d28e95e850f39f221803d4b
AlbertoArochal <arochaldev@gmail.com>
2023-04-21 22:02:16 +0200

    added header

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,11 @@
+import { HomePage } from "../pages/home";
+import { Header } from "../header/header";
+
+export const Layout = () => {
+    return (
+        <>
+            <Header />
+            <HomePage />
+        </>
+    );
+};
